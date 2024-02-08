4d632470af1f4b18b4ae10b6b45abf8ad762c934
vasyagun <63818342+vasyagun@users.noreply.github.com>
2024-02-08 12:39:41 +0300

    Update setup_mrdn.sh

--- setup_mrdn.sh
+++ setup_mrdn.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-apt update && apt install -y curl git screen
+apt update && apt install -y git curl screen
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 16
 
-git clone https://github.com/TrueCarry/JettonGramGpuMiner.git
-cd JettonGramGpuMiner
-echo "SEED=$SEED" > config.txt
-echo "TONAPI_TOKEN=$TONAPI_TOKEN" >> config.txt
+git clone https://github.com/vasyagun/jetton_scripts.git && cd jetton_scripts
+
 npm install
 
-screen -dmS mining bash -c "node send_multigpu.js --api $API_TYPE --bin ./pow-miner-cuda --givers 1000 --gpu-count $GPU_COUNT"
+for ((i=0; i<$GPU_COUNT; i++))
+do
+cat <<EOF > start_gpu_$i.sh
+#!/bin/bash
+while true; do
+  node send_multigpu.js --api $API --bin ./pow-miner-cuda --givers $GIVERS --gpu $i
+  sleep 1
+done
+EOF
+
+chmod +x start_gpu_$i.sh
+screen -dmS miner_$i ./start_gpu_$i.sh
+
+done
 
