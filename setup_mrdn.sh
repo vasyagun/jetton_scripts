d6d65d653225f7cd8d5e7c8114241b8b5f7c3c1e
vasyagun <63818342+vasyagun@users.noreply.github.com>
2024-02-08 12:32:47 +0300

    Update setup_mrdn.sh

--- setup_mrdn.sh
+++ setup_mrdn.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+apt update && apt install -y curl git screen
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 16
+
+git clone https://github.com/TrueCarry/JettonGramGpuMiner.git
+cd JettonGramGpuMiner
+echo "SEED=$SEED" > config.txt
+echo "TONAPI_TOKEN=$TONAPI_TOKEN" >> config.txt
+npm install
+
+screen -dmS mining bash -c "node send_multigpu.js --api $API_TYPE --bin ./pow-miner-cuda --givers 1000 --gpu-count $GPU_COUNT"
 
